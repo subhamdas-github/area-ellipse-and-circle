d75dbeb814f8716e81edebe343e62d9663040194
Subham Das <57589556+subhamdas-github@users.noreply.github.com>
2023-05-06 08:51:46 +0530

    Update area_of_ellipse_and_circle.py

--- area_of_ellipse_and_circle.py
+++ area_of_ellipse_and_circle.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from descartes import PolygonPatch
 from shapely.geometry import Point
 from shapely.affinity import scale, rotate
-
+import numpy
 
 # this fucntion returns generated ellipse. Circle is also type of Ellipse.
 def ellipse_generator(center, axes, inclination):
@@ -11,23 +11,19 @@ def ellipse_generator(center, axes, inclination):
     return _ellipse
 
 # variables
-_radius_r = 2.8; _vertex_a = 3.78; _covertex_b = 2; _xcenter_h = 0; _ycenter_k = 2.22
+_circle_radius = 1.78; _ellipse_vertex = 3.19; _ellipse_co_vertex = 2; _circle_centerX = 0; _circle_centerY = 2.22; _ellipse_centerX =3; _ellipse_centerY = 2
+_offset = 5
 
 # the following fucntions will generate the ellipses for you.
-CIRCLE = ellipse_generator((_xcenter_h, _ycenter_k), (_radius_r, _radius_r), 0) # circle equation = (x - xc)**2 + (y - yc)**2 - r**2
-ELLIPSE = ellipse_generator((0, 0), (_vertex_a, _covertex_b), 0) 
+CIRCLE = ellipse_generator((_circle_centerX, _circle_centerY), (_circle_radius, _circle_radius), 0) # circle equation = (x - xc)**2 + (y - yc)**2 - r**2
+ELLIPSE = ellipse_generator((_ellipse_centerX, _ellipse_centerY), (_ellipse_vertex, _ellipse_co_vertex), 0) 
 
 """
 IMPORTANT
-
-actual ellipse equation = ((x-h)/a)**2 + ((y-k)/b)**2 - 1
-
-In that case modify the line '18' to ELLIPSE = ellipse_generator((h, k), (_vertex_a, _covertex_b), 0) 
-
-If you want to rotate the ellipse to any direction then modify the same to ELLIPSE = ellipse_generator((h, k), (_vertex_a, _covertex_b), <input_value>) 
-
+==========
+If you want to rotate the ellipse to any direction then modify line 19 to 
+ELLIPSE = ellipse_generator((_ellipse_centerX, _ellipse_centerY), (_ellipse_vertex, _ellipse_co_vertex), <your-input>)
 """
-
 
 # Patch the polygons and inner intersection
 _e1 = PolygonPatch(CIRCLE, facecolor='#99ccff', edgecolor='black', linewidth=1)
@@ -38,12 +34,12 @@ if CIRCLE.intersects(ELLIPSE):
     inter = PolygonPatch(_intersect, facecolor='#cc0066',edgecolor='black', linewidth=1, hatch='///')
     _polys.append(inter)
 
-
 # Plotting
-plt.figure()
-_ax = plt.gca()
+_ax=plt.gca()
 [_ax.add_artist(item) for item in _polys]
-_ax.set_xlim(-7, 7)
-_ax.set_ylim(0, 10)
+_max = numpy.max(numpy.array([_circle_radius, _circle_centerX, _circle_centerY, _ellipse_centerX, _ellipse_centerY, _ellipse_co_vertex, _ellipse_vertex]))
+_ax.set_xlim(-_max, _max*2+_offset)
+_ax.set_ylim(-_max, _max*2+_offset)
+_ax.set_aspect('equal', adjustable='box')
+plt.title('Your content here', fontsize=15, color='green', fontstyle='italic')
 plt.show()
-
